8a832be42a230639f5d268cbf71c1b1162b18a88
NBryant83 <73191760+NBryant83@users.noreply.github.com>
2023-04-26 09:48:22 -0400

    started working on job.service.ts

--- src/app/services/job.service.ts
+++ src/app/services/job.service.ts
@@ -6,10 +6,23 @@ import { Job } from '../shared/job';
 })
 export class JobService {
 
-  JobList: Job[] = [];
 
+  
+  JobList: Job[] = [];
+  
   constructor() { 
     this.JobList.push(new Job(2,'Painter', 'Painting', 25.5, 3)), 
     this.JobList.push(new Job(4,'Builder', 'Building', 20.5, 5))
   }
+  
+  // TODO
+  getJob(theJobId: number) {
+    
+    // return single job that gets clicked on/edit
+    console.log(`The Job ID is: ${theJobId}`)
+    return theJobId
+
+  }
+
+
 }
