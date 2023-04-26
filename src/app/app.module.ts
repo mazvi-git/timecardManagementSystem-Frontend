c7958b57a5af864f81ccdc40e5b3855e1940c52c
NBryant83 <73191760+NBryant83@users.noreply.github.com>
2023-04-26 08:45:01 -0400

    refactored component folder to house folders for each component along with their children. Started working on routes in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TimecardListComponent } from './components/timecard-list/timecard-list.component';
-import { TimecardFormComponent } from './components/timecard-form/timecard-form.component';
-import { JobFormComponent } from './components/job-form/job-form.component';
-import { MachineFormComponent } from './components/machine-form/machine-form.component';
-import { JobListComponent } from './components/job-list/job-list.component';
-import { MachineListComponent } from './components/machine-list/machine-list.component';
+import { TimecardListComponent } from './components/timecard/timecard-list/timecard-list.component';
+import { TimecardFormComponent } from './components/timecard/timecard-form/timecard-form.component';
+import { JobFormComponent } from './components/job/job-form/job-form.component';
+import { MachineFormComponent } from './components/machine/machine-form/machine-form.component';
+import { JobListComponent } from './components/job/job-list/job-list.component';
+import { MachineListComponent } from './components/machine/machine-list/machine-list.component';
+import { Routes } from '@angular/router';
+import { JobService } from './services/job.service';
+import { MachineService } from './services/machine.service';
+import { TimecardService } from './services/timecard.service';
+import { JobDetailsComponent } from './components/job/job-details/job-details.component';
+
+
+const routes: Routes = [
+  {path: 'jobs/:id', component: JobListComponent},
+  {path: 'machines/:id', component: MachineListComponent},
+  {path: 'jobs', component: JobListComponent},
+  {path: 'machines', component: MachineListComponent}
+]
 
 
 @NgModule({
@@ -20,12 +33,17 @@ import { MachineListComponent } from './components/machine-list/machine-list.com
     MachineFormComponent,
     JobListComponent,
     MachineListComponent,
+    JobDetailsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    JobService, MachineService, TimecardService
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
